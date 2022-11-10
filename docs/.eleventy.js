f161d4474aefb82aa589fddcd5d86a567c7cb043
Mirnes Glamočić <mirnes.glamocic@gmail.com>
2022-11-10 06:12:42 +0100

    Add files for publishing 11ty

---
+++ docs/.eleventy.js
@@ -0,0 +1,9 @@
+module.exports = eleventyConfig => {
+    eleventyConfig.addPassthroughCopy("src/")
+    return {
+        dir: {
+            input: "src",
+            output: "docs"
+        }
+    }
+}
